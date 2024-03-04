0be776f8ca0e2358b69b85e7c204625d7c616da6
Alex Severin <severinalexeyv@gmail.com>
2024-03-04 19:41:41 +0300

    add sep folder for neon ker4x4

---
+++ src/kernels/neon/ker4x4/mod.rs
@@ -0,0 +1,48 @@
+#[cfg(target_arch = "aarch64")]
+mod microkernel;
+
+use core::marker::PhantomData;
+
+/// Available only for the `aarch64` target.
+#[derive(Debug, Clone, Copy)]
+pub struct NeonKernel4x4<T> {
+    marker: PhantomData<T>,
+}
+
+impl<T> NeonKernel4x4<T> {
+    /// # Safety
+    ///
+    /// The caller must ensure that the created kernel will only be used in an
+    /// environment with `neon` support.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use microgemm::kernels::NeonKernel4x4;
+    ///
+    /// let kernel = if cfg!(target_feature = "neon") {
+    ///     unsafe { NeonKernel4x4::<f32>::new() }
+    /// } else {
+    ///     panic!("neon target feature is not enabled");
+    /// };
+    /// ```
+    #[cfg(not(doctest))]
+    pub const unsafe fn new() -> Self {
+        Self {
+            marker: PhantomData,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn _check_new_neon() {
+        use crate::kernels::NeonKernel4x4;
+
+        let _kernel = if cfg!(target_feature = "neon") {
+            unsafe { NeonKernel4x4::<f32>::new() }
+        } else {
+            panic!("neon target feature is not enabled");
+        };
+    }
+}
