0e408ed478df6b25117c914f651b817bcc776858
Alex Severin <severinalexeyv@gmail.com>
2024-10-20 18:53:54 +0300

    fix import

--- src/kernels/neon/mod.rs
+++ src/kernels/neon/mod.rs
@@ -5,12 +5,12 @@ mod ker8x8;
 mod simd_mock;
 
 mod simd {
-    #[cfg(not(any(kani, miri)))]
-    pub use core::arch::aarch64::{
+    #[cfg(any(kani, miri))]
+    pub use super::simd_mock::{
         vaddq_f32, vfmaq_f32, vfmaq_laneq_f32, vld1q_f32, vmovq_n_f32, vmulq_n_f32, vst1q_f32,
     };
-    #[cfg(any(kani, miri))]
-    pub use simd_mock::{
+    #[cfg(not(any(kani, miri)))]
+    pub use core::arch::aarch64::{
         vaddq_f32, vfmaq_f32, vfmaq_laneq_f32, vld1q_f32, vmovq_n_f32, vmulq_n_f32, vst1q_f32,
     };
 }
